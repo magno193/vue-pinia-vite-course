2d19eba9562636251bbd670cc100c25355bc3831
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-14 15:41:19 -0300

    feat: 01 usando useRoute

--- 01-composition-api-basic/src/views/PostDetailView.vue
+++ 01-composition-api-basic/src/views/PostDetailView.vue
@@ -2,6 +2,7 @@
   <div class="post-detail">
     <h1>Uma página de detalhes de postagem</h1>
     <p>Mostre o conteúdo da postagem com ID de {{ $route.params.id }} neste espaço</p>
+    <div><button @click="showPostId">Mostrar ID de Postagem</button></div>
     <p>
       <RouterLink to="/posts">&lt; Back</RouterLink>
     </p>
@@ -9,4 +10,11 @@
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute() // mesmo que this.$route em options
+
+const showPostId = () => {
+  alert(`O ID desta postagem é ${route.params.id}`)
+}
 </script>

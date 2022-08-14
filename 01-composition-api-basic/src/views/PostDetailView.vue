42343cc3c97c8f983c8ca27ae6998a7b5bd182a1
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-14 15:35:43 -0300

    feat: 01 detalhando postagens e usando $route

--- 01-composition-api-basic/src/views/PostDetailView.vue
+++ 01-composition-api-basic/src/views/PostDetailView.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="post-detail">
     <h1>Uma página de detalhes de postagem</h1>
+    <p>Mostre o conteúdo da postagem com ID de {{ $route.params.id }} neste espaço</p>
+    <p>
+      <RouterLink to="/posts">&lt; Back</RouterLink>
+    </p>
   </div>
 </template>
 
 <script setup>
-import { vAutofocus } from '@/directives/vAutofocus';
 </script>
--- 01-composition-api-basic/src/views/PostsView.vue
+++ 01-composition-api-basic/src/views/PostsView.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="posts">
     <h1>Postagens</h1>
-    <textarea name="" id="" cols="30" rows="10" v-autofocus />
+
+    <ul>
+      <li>
+        <Router-Link to="/posts/id1">Post 1</Router-Link>
+      </li>
+      <li>
+        <Router-Link to="/posts/id2">Post 2</Router-Link>
+      </li>
+      <li>
+        <Router-Link to="/posts/id3">Post 3</Router-Link>
+      </li>
+    </ul>
+
+    <textarea v-autofocus></textarea>
   </div>
 </template>
 

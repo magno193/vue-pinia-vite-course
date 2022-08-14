f2b2e73eadcfc72ba2a204b26c1de047cd6991bf
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-13 21:50:00 -0300

    feat: 01 contador pelo options api

--- 01-composition-api-basic/src/views/HomeView.vue
+++ 01-composition-api-basic/src/views/HomeView.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">
     <div>
-      <button class="btn">-</button>
-      <span class="counter">0</span>
-      <button class="btn">+</button>
+      <button class="btn" @click="decreaseCounter">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button class="btn" @click="increaseCounter">+</button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    counter: 0
+  }),
+  methods: {
+    increaseCounter() {
+      this.counter++
+    },
+    decreaseCounter() {
+      this.counter--
+    }
+  }
+}
+</script>
 
 <style>
 .home {

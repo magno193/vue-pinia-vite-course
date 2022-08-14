f4a91d3551fb2a2ebb6a5d0e139d73cc544eaef5
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-13 22:08:30 -0300

    feat: 01 two-way data binding com v-model

--- 01-composition-api-basic/src/views/HomeView.vue
+++ 01-composition-api-basic/src/views/HomeView.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="home">
+
+    <h3 class="title">{{ counterTitle }}:</h3>
+
     <div>
       <button class="btn" @click="decreaseCounter">-</button>
       <span class="counter">{{ counter }}</span>
       <button class="btn" @click="increaseCounter">+</button>
     </div>
+
+    <div class="edit">
+      <h4>Editar título</h4>
+      <input v-model="counterTitle" type="text">
+    </div>
   </div>
 </template>
+
 <script setup>
 import { ref } from 'vue';
-const counter = ref(0);
+const counter = ref(0),
+  counterTitle = ref("Meu contador")
 const increaseCounter = () => {
   counter.value++
 }
@@ -65,5 +75,9 @@ export default {
 .counter {
   font-size: 3rem;
   margin: 1rem;
+}
+
+.edit {
+  margin-top: 2rem;
 }
 </style>

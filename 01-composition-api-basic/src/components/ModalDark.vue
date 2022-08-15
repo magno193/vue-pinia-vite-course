cc6396968aa33d423069c931a5daffbe825721f1
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-15 10:23:41 -0300

    feat: 01 uso de componentes dinamicos <component>

---
+++ 01-composition-api-basic/src/components/ModalDark.vue
@@ -0,0 +1,53 @@
+<template>
+    <teleport to=".modals-container">
+        <div
+          v-if="modelValue"
+          class="modal-dark "
+        >
+            <h1>{{ title }}</h1>
+            <slot />
+            <button @click="handleButtonClick">Esconder modal</button>
+        </div>
+    </teleport>
+</template>
+
+<script setup>
+//#region imports
+//#endregion
+
+//#region props
+const props = defineProps({
+    modelValue: { // prop especial para v-model
+        type: Boolean,
+        default: false,
+    },
+    title: {
+        type: String,
+        default: 'Nenhum título especificado'
+    }
+})
+//#endregion
+
+//#region emits
+const emits = defineEmits(['update:modelValue']) // this.$emit('hideModal')
+//#endregion
+
+//#region btn cick
+const handleButtonClick = () => {
+    emits('update:modelValue', false)
+}
+//#endregion
+</script>
+
+<style>
+.modal-dark {
+    background: #333;
+    color: white;
+    padding: 1rem;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
--- 01-composition-api-basic/src/views/ModalsView.vue
+++ 01-composition-api-basic/src/views/ModalsView.vue
@@ -2,21 +2,37 @@
     <div class="modals">
         <h1>Modals</h1>
         <button @click="showModal = true">Mostrar modal</button>
-        <Modal
-          v-model="showModal"
+        <div>
+            <label>Mostrar modal dark?
+                <input
+                  type="checkbox"
+                  v-model="showDarkModals"
+                >
+            </label>
+        </div>
+        <!-- <component
+          :is="Modal"
           title="Isto é uma modal"
+          v-model="showModal"
+        > -->
+        <component
+          :is="showDarkModals ? ModalDark : Modal"
+          title="Isto é uma modal"
+          v-model="showModal"
         >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, illo?
-        </Modal>
+        </component>
     </div>
 </template> 
 <script setup>
 //#region imports
 import { ref } from 'vue'
 import Modal from '../components/Modal.vue'
+import ModalDark from '../components/ModalDark.vue'
 //#endregion
 
 //#region modals
 const showModal = ref(false)
+const showDarkModals = ref(false)
 //#endregion
 </script>

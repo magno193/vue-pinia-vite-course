f9f984234650abbb323e8c0da618ae56f036ed9d
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-15 15:36:58 -0300

    feat: 01 criação e uso de composable para counter

---
+++ 01-composition-api-basic/src/use/useCounter.js
@@ -0,0 +1,48 @@
+import { reactive, ref, computed, watch, onMounted, nextTick } from 'vue';
+
+export function useCounter() {
+    const counterData = reactive({
+        counter: 0,
+        title: 'Meu contador'
+    })
+    const accAmount = ref(1)
+
+    const oddOrEvenFunc = () => {
+        return counterData.counter % 2 === 0
+            ? 'Este contador é par'
+            : 'Este contador é ímpar'
+    }
+    const oddOrEven = computed(oddOrEvenFunc)
+
+    const increaseCounter = async (amount = 1) => {
+        counterData.counter += amount
+
+        /**
+         * Mesmo que o this.$nextTick
+         * Executado após um update em DOM
+         * Pode ser assíncorno
+         */
+
+        await nextTick(() => {
+            console.log("Faça algo relacionado ao increaseCounter");
+        })
+    }
+    const decreaseCounter = (amount = 1) => {
+        counterData.counter -= amount
+    }
+
+    const countSource = () => counterData.counter,
+        countHandler = (counter, old) => counter === 20 ? alert('Você atingiu o marco 20!') : null
+    watch(countSource, countHandler)
+
+    onMounted(() => {
+        console.log('Coisas relacionadas ao contador.');
+    })
+    return {
+        counterData,
+        oddOrEven,
+        increaseCounter,
+        decreaseCounter,
+        accAmount
+    }
+}
--- 01-composition-api-basic/src/views/HomeView.vue
+++ 01-composition-api-basic/src/views/HomeView.vue
@@ -22,7 +22,8 @@
 
 <script setup>
 //#region imports
-import { reactive, ref, computed, watch, onMounted, nextTick } from 'vue';
+import {  ref, onMounted } from 'vue';
+import { useCounter} from '../use/useCounter'
 import { vAutofocus } from '@/directives/vAutofocus'
 //#endregion
 
@@ -36,43 +37,7 @@ onMounted(() => {
 //#endregion
 
 //#region counter data
-const counterData = reactive({
-  counter: 0,
-  title: 'Meu contador'
-})
-const accAmount = ref(1)
-
-const oddOrEvenFunc = () => {
-  return counterData.counter % 2 === 0
-    ? 'Este contador é par'
-    : 'Este contador é ímpar'
-}
-const oddOrEven = computed(oddOrEvenFunc)
-
-const increaseCounter = async (amount = 1) => {
-  counterData.counter += amount
-
-  /**
-   * Mesmo que o this.$nextTick
-   * Executado após um update em DOM
-   * Pode ser assíncorno
-   */
-  
-  await nextTick(() => {
-    console.log("Faça algo relacionado ao increaseCounter");
-  })
-}
-const decreaseCounter = (amount = 1) => {
-  counterData.counter -= amount
-}
-
-const countSource = () => counterData.counter,
-  countHandler = (counter, old) => counter === 20 ? alert('Você atingiu o marco 20!') : null
-watch(countSource, countHandler)
-
-onMounted(() => {
-  console.log('Coisas relacionadas ao contador.');
-})
+const { accAmount, counterData, decreaseCounter, increaseCounter, oddOrEven } = useCounter()
 //#endregion
 
 //#region directives

138a6ca227787749874b6456ac22c1615aeec813
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-16 15:42:10 -0300

    feat: 02 cards de bulma para view de notes

--- 02-noteballs/src/views/ViewNotes.vue
+++ 02-noteballs/src/views/ViewNotes.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="notes">
-    <h1>Notas</h1>
+    <div v-for="i in 3" :key="i" class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ea voluptas
+          dolore, voluptatibus velit eos iusto dignissimos accusantium molestias, ut
+          magnam, debitis provident id harum? Explicabo placeat molestiae cum quos.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
 

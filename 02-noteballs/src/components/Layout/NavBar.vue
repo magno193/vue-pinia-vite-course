5c3bfba24de8e8c601aa5784466f58bed5fd0a85
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-16 14:09:40 -0300

    feat: 02 corpo de NavBar com css Bulma

--- 02-noteballs/src/components/Layout/NavBar.vue
+++ 02-noteballs/src/components/Layout/NavBar.vue
@@ -1,9 +1,7 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-      </a>
+      <div class="navbar-item is-size-4 is-family-monospace">Noteballs</div>
 
       <a
         role="button"
@@ -19,38 +17,22 @@
     </div>
 
     <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item"> Home </a>
-
-        <a class="navbar-item"> Documentation </a>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link"> More </a>
-
-          <div class="navbar-dropdown">
-            <a class="navbar-item"> About </a>
-            <a class="navbar-item"> Jobs </a>
-            <a class="navbar-item"> Contact </a>
-            <hr class="navbar-divider" />
-            <a class="navbar-item"> Report an issue </a>
-          </div>
-        </div>
-      </div>
-
       <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light"> Log in </a>
-          </div>
-        </div>
+        <RouterLink :to="{ name: 'notes' }" class="navbar-item" active-class="is-active">
+          Notas
+        </RouterLink>
+        <RouterLink :to="{ name: 'stats' }" class="navbar-item" active-class="is-active">
+          Estatísticas
+        </RouterLink>
       </div>
     </div>
   </nav>
 </template>
 
-<script setup></script>
+<script setup>
+//#region imports
+import { RouterLink } from "vue-router";
+//#endregion
+</script>
 
 <style lang="scss" scoped></style>

b6b36e1639a7a879c62f576099a2d6abbb801d2e
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-18 10:27:11 -0300

    refactor: 02 componentizado formulário de create/edit de notas

---
+++ 02-noteballs/src/components/Notes/AddEditNote.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card has-background-success-dark p-4 mb-5">
+    <div class="field">
+      <div class="control">
+        <textarea
+          class="textarea"
+          placeholder="Adicionar nova nota"
+          ref="noteInputRef"
+        ></textarea>
+      </div>
+    </div>
+
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <slot name="buttons" />
+      </div>
+    </div>
+  </div>
+</template>
--- 02-noteballs/src/views/ViewNotes.vue
+++ 02-noteballs/src/views/ViewNotes.vue
@@ -1,29 +1,16 @@
 <template>
   <div class="notes">
-    <div class="card has-background-success-dark p-4 mb-5">
-      <div class="field">
-        <div class="control">
-          <textarea
-            v-model="noteInput"
-            class="textarea"
-            placeholder="Adicionar nova nota"
-            ref="noteInputRef"
-          ></textarea>
-        </div>
-      </div>
-
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <button
-            @click="addNote"
-            :disabled="!noteInput"
-            class="button is-link has-background-success"
-          >
-            Adicionar Nova Nota
-          </button>
-        </div>
-      </div>
-    </div>
+    <AddEditNote>
+      <template #buttons>
+        <button
+          @click="addNote"
+          :disabled="!noteInput"
+          class="button is-link has-background-success"
+        >
+          Adicionar Nova Nota
+        </button>
+      </template>
+    </AddEditNote>
 
     <TheNote :note="note" v-for="note in storeNotes.notes" :key="note.id" />
   </div>
@@ -33,6 +20,7 @@
 //#region imports
 import { ref } from "vue";
 import TheNote from "@/components/Notes/TheNote.vue";
+import AddEditNote from "@/components/Notes/AddEditNote.vue";
 import { useStoreNotes } from "../stores/storeNotes";
 //#endregion
 

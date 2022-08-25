1d3ea2d3cc14cde41a5a56a60a9ae2587885aa98
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-25 00:02:13 -0300

    chore: 03 instalação e config firebase

---
+++ 03-noteballs-auth/src/js/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0LQDwJ4CWaix4JQK_-2u1CQu4cvFLUBA",
+  authDomain: "noteballs-93e5b.firebaseapp.com",
+  projectId: "noteballs-93e5b",
+  storageBucket: "noteballs-93e5b.appspot.com",
+  messagingSenderId: "652519093570",
+  appId: "1:652519093570:web:5df49aa81367d12da804db"
+};
+
+const app = initializeApp(firebaseConfig);

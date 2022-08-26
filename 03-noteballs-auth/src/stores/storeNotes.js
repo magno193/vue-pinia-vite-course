ce0d61e10ea082c2e462a56d73b7933571f3a100
Alexandre Ferreira <alexmlf93@gmail.com>
2022-08-26 00:07:55 -0300

    refactor: 03 utiliando onSnapshot para update de base real time

--- 03-noteballs-auth/src/stores/storeNotes.js
+++ 03-noteballs-auth/src/stores/storeNotes.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 import { db } from '../js/firebase'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, onSnapshot } from 'firebase/firestore'
 
 export const useStoreNotes = defineStore('storeNotes', {
   state: () => ({
@@ -19,18 +19,26 @@ export const useStoreNotes = defineStore('storeNotes', {
   },
   actions: {
     async getNotes() {
-      const query = await getDocs(collection(db, 'notes'))
-      query.forEach((doc) => {
-        let data = doc.data(); // {content: <...>}
-        let dataKey = Object.keys(data).at(0) // "content"
+      //#region real time db
+      /**
+       * onSnapshot: as mudanças fora da página são refletidas em real time
+       */
+      const query = collection(db, 'notes');
+      onSnapshot(query, querySnapshot => {
+        let notes = []
+        querySnapshot.forEach(doc => {
+          let data = doc.data(); // {content: <...>}
+          let dataKey = Object.keys(data).at(0) // "content"
 
-        let note = {
-          id: doc.id,
-          [dataKey]: data[dataKey]
-        }
-
-        this.notes = [...this.notes, note]
+          let note = {
+            id: doc.id,
+            [dataKey]: data[dataKey]
+          }
+          notes.push(note)
+        })
+        this.notes = notes
       })
+      //#endregion
     },
     addNote(content) {
       this.$state.notes.unshift({
